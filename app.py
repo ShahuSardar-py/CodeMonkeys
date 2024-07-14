72190309068c834b2b13ab2f5ff2e6e911964b60
ShahuSardar-py <shahu.sardar4@gmail.com>
2024-07-14 16:15:53 +0530

    module 2- faculty and student summary

--- app.py
+++ app.py
@@ -12,10 +12,47 @@ import io
 def combine_excel(file1, file2):
     df1 = pd.read_excel(file1, engine='openpyxl')
     df2 = pd.read_excel(file2, engine='openpyxl')
-    #dropping the header (f2)  here 
     combined_df = pd.concat([df1, df2], ignore_index=True)
     return combined_df
 
+#faculty and student diffrentiator
+def role_filter(df):
+    faculty_df = df[df['Role'] == 'faculty']
+    student_df = df[df['Role'] == 'student']
+    return faculty_df, student_df
+
+#column drop
+def drop_columns(df, columns_to_drop):
+    return df.drop(columns=columns_to_drop, errors='ignore')
+
+
+#summary table
+def summary(faculty_df, student_df):
+    summary_data = {
+        'Role': ['Faculty', 'Student'],
+        'Elite + Silver': [
+            (faculty_df['Certificate Type'] == 'Elite+Silver').sum(),
+            (student_df['Certificate Type'] == 'Elite+Silver').sum()
+        ],
+        'Elite Certificate': [
+            (faculty_df['Certificate Type'] == 'Elite').sum(),
+            (student_df['Certificate Type'] == 'Elite').sum()
+        ],
+        'Successfully completed': [
+            (faculty_df['Certificate Type'] == 'Successfully completed').sum(),
+            (student_df['Certificate Type'] == 'Successfully completed').sum()
+        ],
+        'No Certificate': [
+            (faculty_df['Certificate Type'] == 'No Certificate').sum(),
+            (student_df['Certificate Type'] == 'No Certificate').sum()
+        ],
+        'Average Exam Score': [
+            faculty_df['Final Score'].mean(),
+            student_df['Final Score'].mean()
+        ]
+    }
+    summary_df = pd.DataFrame(summary_data)
+    return summary_df
 
 
 
@@ -36,13 +73,9 @@ def combine_excel(file1, file2):
 
 
 
-
-
-
-
-
-#the page
+#the page setup (UI)
 st.set_page_config(page_title="Report Automator")
+#header
 st.title("NPTEL Report Generator")
 st.write("V 1.1.0")
 
@@ -53,13 +86,32 @@ uploaded_file2 = st.sidebar.file_uploader("Upload second Excel file", type=["xls
 
 #sucessful upload
 if uploaded_file1 is not None and uploaded_file2 is not None:
-    st.write("Upload sucessful!")
-    with st.expander("Academic year data: "):
-        combined_df = combine_excel(uploaded_file1, uploaded_file2)
-        st.write(combined_df)
+    st.write("Upload successful!")
+    combined_df = combine_excel(uploaded_file1, uploaded_file2)
     
+    columns_to_drop = ["College Roll no", "Unproctored programming exam score out of 25", "Year of passing"]
+    combined_df = drop_columns(combined_df, columns_to_drop)
+    
+    faculty_df, student_df = role_filter(combined_df)
+    
+    with st.expander("Faculty Data"):
+        st.write(faculty_df)
+
+    with st.expander("Student Data"):
+        st.write(student_df)
+    
+    st.markdown("----")
+    st.write("# Analysis Report")
+
+    summary_df = summary(faculty_df, student_df)
+    st.write("#### Summary Statistics")
+    st.write(summary_df)
+
+    
+
+
 else:
-    st.write("upload two files")
+    st.write("Please upload two files")
 
 
 
